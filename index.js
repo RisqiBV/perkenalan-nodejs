dcb0abb5b90a8fc1522a90aa81faa8621aa61b64
Risqi Bagus <risqibaguspahlevi@gmail.com>
2023-10-19 21:28:25 +0700

    Update 21.30 WIB

---
+++ index.js
@@ -0,0 +1,18 @@
+// function printname(prm){
+//     return `Haiiiii`
+// }
+
+
+// module.exports = {
+
+// }
+
+// const {Client} = require('pg')
+// const client = new Client()
+// await client.connect()
+
+// console res = await client.query('SELECT $1')
+
+// async function main(){
+
+// }
